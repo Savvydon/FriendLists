9e6813e68a053945d6d88133c7203d483623e9bf
Savvydon <donsave4real@gmail.com>
2025-08-25 16:47:48 +0100

    first commit

---
+++ src/Updater.jsx
@@ -0,0 +1,79 @@
+// import { useState } from "react";
+
+// export default function Updater() {
+//   const [names, setNames] = useState([]);
+
+//   function addNames() {
+//     const newName = document.getElementById("friendList").value;
+//     document.getElementById("friendList").value = "";
+//     //setNames((prevN) => [...prevN, newNames]);
+//     setNames([...names, newName]);
+//   }
+
+//   function removeFriend(index) {
+//     setNames(names.filter((_, i) => i !== index));
+//   }
+//   return (
+//     <>
+//       <div>
+//         <input type="text" id="friendList" placeholder="Enter Friends Name" />
+//         <button onClick={addNames}>Add Friend</button>
+//       </div>
+
+//       <h2>List of Friends</h2>
+//       <ol>
+//         {names.map((name, index) => (
+//           <li key={index} onClick={() => removeFriend(index)}>
+//             {name}
+//           </li>
+//         ))}
+//       </ol>
+//     </>
+//   );
+// }
+
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+
+export default function Updater() {
+  const [names, setNames] = useState([]);
+
+  function addNames() {
+    const newName = document.getElementById("friendList").value;
+    document.getElementById("friendList").value = "";
+    //setNames((prevN) => [...prevN, newNames]);
+    setNames([...names, newName]);
+  }
+
+  function removeFriend(index) {
+    setNames(names.filter((_, i) => i !== index));
+  }
+  return (
+    <div className="container">
+      <div>
+        <input
+          type="text"
+          id="friendList"
+          className="input-field"
+          placeholder="Enter Friend's Name"
+        />
+        <button className="add-btn" onClick={addNames}>
+          Add Friend
+        </button>
+      </div>
+      <h2>List of Friends</h2>
+      <ol className="friend-list">
+        {names.map((name, index) => (
+          <li key={index}>
+            {name}
+            <FaTrash
+              className="trash-icon"
+              style={{ cursor: "pointer", color: "red" }}
+              onClick={() => removeFriend(index)}
+            />
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
